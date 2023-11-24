e071654d6e56621af762cec72a249d388bd8e8d2
Rendy Pratama <rendypratama752@gmail.com>
2023-11-25 01:53:33 +0700

    STYLE: clean code, removed unused images

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,15 @@
-var item = document.getElementById("navItem");
-document.getElementsByClassName('hamburgerMenu')[0].addEventListener('click', () =>
+document.getElementsByClassName('hamburgerMenu')[0].addEventListener('click', () => {
+    var item = document.getElementById("navItem");
     item.style.display=="none"?item.style.display="block":item.style.display="none"
-);
+});
 
 ["facebook","github","instagram","linkedin","twitter","youtube"].map(item=>
     document.getElementById("medsosGroup").innerHTML+=`<img src="assets/images/sosialMedia/${item}.png" class="medsos" alt="${item}">`
-);
+)
 
 class Book {
     constructor(){
-        this.books=[];
+        this.books=[]
     }
 
     saveBook(){
@@ -21,17 +21,18 @@ class Book {
     }
 
     loadBook(){
-        this.books = JSON.parse(localStorage.getItem('books'));
+        const getBooks = JSON.parse(localStorage.getItem('books'));
+        if (getBooks) {this.books = JSON.parse(localStorage.getItem('books'))};
         renderBooksHistory();
     }
 
-    addBook(title, author, year, isCompleted){
+    addBook(title, author, year, isComplete){
         const book = {
             id: +new Date(),
             title: title,
             author: author,
             year: year,
-            isCompleted: isCompleted
+            isComplete: isComplete
         }
         this.books.push(book);
         this.saveBook(); //
@@ -39,7 +40,7 @@ class Book {
     }
 
     changeStatus(index){
-        this.books[index].isCompleted = !this.books[index].isCompleted;
+        this.books[index].isComplete = !this.books[index].isComplete;
         this.saveBook();
         renderBooksHistory();
     }
@@ -63,11 +64,7 @@ class Book {
 const book = new Book();
 document.addEventListener('DOMContentLoaded', () => {
     if(isStorageExist()) {book.loadBook()}
-});
-
-function isStorageExist() {
-    return typeof (Storage) === undefined?(alert('Browser kamu tidak mendukung local storage'),false):true;
-}
+})
 
 function isStorageExist() {
     return (typeof (Storage) === undefined)?false:true;
@@ -81,55 +78,50 @@ function renderBooksHistory(listBook = JSON.parse(localStorage.getItem('books'))
     finishedListBook.innerHTML='';
     deletedBookContainer.innerHTML='';
     var ongoingIndex = 1; var finishedIndex = 1;
-    // const listBook = JSON.parse(localStorage.getItem('books'));
-    if (listBook.filter(item => item.isCompleted==false)==0){
-        ongoingListBook.innerHTML+='<img id="notFound" src="assets/images/noResults.gif" alt="no results">'
-    }
-    if (listBook.filter(item => item.isCompleted==true)==0){
-        finishedListBook.innerHTML+='<img id="notFound" src="assets/images/noResults.gif" alt="no results">'
-    }
-    listBook.map((item,index) => {
-        item.isCompleted?(
-        finishedListBook.innerHTML+=
-        `<div class="bookContainer" id="${item.id}">
-            <h3 class="finishedIndex">${ongoingIndex}</h3>
-            <div class="bookData">
-                <h3 id="title">${item.title}</h3>
-                <p id="author">Author : ${item.author}</p>
-                <p id="year">Year   : ${item.year}</p>
-            </div>
-            <div class="bookButton">
-                <button id="buttonOngoing" onclick="book.changeStatus(${index})">Ongoing</button>
-                <div class="buttonAction">
-                    <button class="buttonEdit" onclick="showEdit(${item.id})">Edit</button>
-                    <button class="buttonDelete" onclick="showConfirmation(${item.id})">Delete</button>
+    const noResultsFinished = '<img id="notFound" src="assets/images/noResults.gif" alt="no results">';
+    const noResultsOngoing = '<img id="notFound" src="assets/images/noResults.gif" alt="no results">';
+    if (listBook==null){
+        finishedListBook.innerHTML=noResultsFinished; ongoingListBook.innerHTML=noResultsOngoing}
+    else {
+        if (listBook.filter(item => item.isComplete==false).length==0){ongoingListBook.innerHTML=noResultsOngoing}
+        if (listBook.filter(item => item.isComplete==true).length==0){finishedListBook.innerHTML=noResultsFinished}
+
+        const generateBookContainer = (item, index, indexBook) => {
+            return (`
+                <div class="bookContainer" id=${item.id}>
+                <h3 class=${item.isComplete?'finishedIndex':'ongoingIndex'}>${index}</h3>
+                <div class="bookData">
+                    <h3 id="title">${item.title}</h3>
+                    <p id="author">Author : ${item.author}</p>
+                    <p id="year">Year   : ${item.year}</p>
                 </div>
-            </div>
-        </div>`,ongoingIndex++)
-        :
-        (ongoingListBook.innerHTML+=
-        `<div class="bookContainer" id="${item.id}">
-            <h3 class="ongoingIndex">${finishedIndex}</h3>
-            <div class="bookData">
-                <h3 id="title">${item.title}</h3>
-                <p id="author">Author : ${item.author}</p>
-                <p id="year">Year   : ${item.year}</p>
-            </div>
-            <div class="bookButton">
-                <button id="buttonFinished" onclick="book.changeStatus(${index})">Finished</button>
-                <div class="buttonAction">
-                    <button class="buttonEdit" onclick="showEdit(${item.id})">Edit</button>
-                    <button class="buttonDelete" onclick="showConfirmation(${item.id})">Delete</button>
+                <div class="bookButton">
+                    <button id=${item.isComplete?'buttonOngoing':'buttonFinished'} onclick="book.changeStatus(${indexBook})">${item.isComplete?'Ongoing':'Finished'}</button>
+                    <div class="buttonAction">
+                        <button class="buttonEdit" onclick="showEdit(${item.id})">Edit</button>
+                        <button class="buttonDelete" onclick="showConfirmation(${item.id})">Delete</button>
+                    </div>
                 </div>
-            </div>
-        </div>`,finishedIndex++);
-    });
+            </div>`
+            )
+        }
+
+        listBook.map((item,index) => {
+            item.isComplete?(
+            finishedListBook.innerHTML+=
+            generateBookContainer(item, finishedIndex, index),finishedIndex++)
+            :
+            (ongoingListBook.innerHTML+=
+            generateBookContainer(item, ongoingIndex, index),ongoingIndex++);
+        })
+    }
+
     const deletedBook = JSON.parse(localStorage.getItem('deletedBook'));
     deletedBook?
     deletedBookContainer.innerHTML=
     `<div id="deletedBookContainer" class="bookContainer">
         <div class="bookData">
-            <h3 id="title">${deletedBook.title} (${deletedBook.isCompleted?'Finished':'Ongoing'})</h3>
+            <h3 id="title">${deletedBook.title} (${deletedBook.isComplete?'Finished':'Ongoing'})</h3>
             <p id="author">Author : ${deletedBook.author}</p>
             <p id="year">Year   : ${deletedBook.year}</p>
         </div>
@@ -137,7 +129,14 @@ function renderBooksHistory(listBook = JSON.parse(localStorage.getItem('books'))
             <button id="buttonUndo">Undo</button>
             <button id="buttonTrash">Trash</button>
         </div>
-    </div>`:null;
+    </div>`
+    :
+    deletedBookContainer.innerHTML=
+    `<div id="deletedBookContainer" class="bookContainer">
+        <div class="bookData">
+            <h3 id="Title">Tidak ada data</h3>
+        </div>
+    </div>`;
 
     const buttonUndo = document.getElementById('buttonUndo');
     const buttonTrash = document.getElementById('buttonTrash');
@@ -146,7 +145,7 @@ function renderBooksHistory(listBook = JSON.parse(localStorage.getItem('books'))
         buttonUndo.addEventListener('click', () => {
             const deletedBook = JSON.parse(localStorage.getItem('deletedBook'));
             localStorage.removeItem('deletedBook');
-            book.addBook(deletedBook.title, deletedBook.author, deletedBook.year, deletedBook.isCompleted);
+            book.addBook(deletedBook.title, deletedBook.author, deletedBook.year, deletedBook.isComplete);
             renderBooksHistory();
         })
     }
@@ -167,7 +166,7 @@ form.addEventListener('submit', (e) => {
     const isComplete = document.getElementById('isComplete').checked;
     e.preventDefault();
     book.addBook(title, author, year, isComplete);
-});
+})
 
 function showEdit(id) {
     renderBooksHistory();
@@ -190,7 +189,6 @@ function showEdit(id) {
     const title = bookDataEdit.getElementsByClassName('title')[0];
     const author = bookDataEdit.getElementsByClassName('author')[0];
     const year = bookDataEdit.getElementsByClassName('year')[0];
-
     title.defaultValue=selectedBook[0].title;
     author.defaultValue=selectedBook[0].author;
     year.defaultValue=selectedBook[0].year;
@@ -200,14 +198,11 @@ function showEdit(id) {
     buttonEdit.innerText='Save';
     buttonEdit.classList.add('buttonSave');
     const buttonSave = bookDataEdit.getElementsByClassName('buttonSave')[0];
-    // console.log(indexSelectedBook, title.value, author.value, year.value);
-
     buttonSave.addEventListener('click', () => {
         buttonSave.classList.replace('buttonSave','buttonEdit');
         const titleValue = title.value; const authorValue = author.value; const yearValue = year.value;
-        // console.log(indexSelectedBook, titleValue, authorValue, yearValue);
         book.editBook(indexSelectedBook, titleValue, authorValue, yearValue);
-    });
+    })
 }
 
 function showConfirmation(id) {
@@ -228,12 +223,8 @@ function showConfirmation(id) {
 }
 
 function searchBook(criteria) {
-    // console.log(criteria);
-    // console.log(localStorage.getItem('books'));
     const listBook = JSON.parse(localStorage.getItem('books'));
-    console.log(listBook.filter((item => item.title.includes(criteria))));
     const selectedBook = listBook.filter((item => item.title.toLowerCase().includes(criteria.toLowerCase())));
-    console.log(selectedBook);
     renderBooksHistory(selectedBook);
 }
 
@@ -247,4 +238,4 @@ document.getElementById('searchInput').addEventListener('keypress', e => {
         const criteria = document.getElementById('searchInput').value;
         searchBook(criteria);
     }
-});
+})
